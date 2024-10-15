b92dc65a9b4e513b5734b302216bd9429dbb976c
Yaseer123 <yaseerarafat230@gmail.com>
2024-10-15 15:28:50 +0600

    admin

---
+++ src/app/admin/aboutus/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutUsManagement from "~/app/_components/AboutUsForm";
+
+const AboutUsPage = () => {
+  return (
+    <div>
+      <AboutUsManagement />
+    </div>
+  );
+};
+
+export default AboutUsPage;
---
+++ src/app/admin/seo/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SEOComponent from "~/app/_components/SEOComponent";
+
+const SEOPage = () => {
+  return <SEOComponent />;
+};
+
+export default SEOPage;
